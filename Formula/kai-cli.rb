385cce20b750ea5cc8d3ad59d3e47b799a06bc0f
Ramin Keene <raminkeene@gmail.com>
2020-08-25 12:51:12 -0700

    no sha

--- Formula/kai-cli.rb
+++ Formula/kai-cli.rb
@@ -6,11 +6,11 @@ class KaiCli < Formula
 
   if OS.mac?
     url "https://github.com/ramin/oh/releases/download/0.0.4/kai.amd64.tar.gz"
-    sha256 "52059721375a7fd90ca52f2dd8bfb4599321c232c9c02f55c6ae237e9619341e"
+    # sha256 "52059721375a7fd90ca52f2dd8bfb4599321c232c9c02f55c6ae237e9619341e"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/ramin/oh/releases/download/0.0.4/kai.amd64.tar.gz"
-      sha256 "a859fb74c8c9d1f82a5e4b31292180fbf27a1c7348ed05d7926d61855123ce8b"
+      # sha256 "a859fb74c8c9d1f82a5e4b31292180fbf27a1c7348ed05d7926d61855123ce8b"
     end
   end
 
